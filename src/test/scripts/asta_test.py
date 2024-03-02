6c361061d62ab1d88608d4cb20eae1902ed6c340
Or <orshkuri2000@gmail.com>
2024-03-03 00:17:02 +0200

    your comment

--- src/test/scripts/asta_test.py
+++
@@ -1,8 +0,0 @@
-from hwetests import asta
-
-if __name__ == '__main__':
-    p_value, statistic, dof = asta.full_algorithm(file_path='../data/ambiguous_data.csv',
-                                                  cutoff_value=4.0)
-    print(f'p-value: {p_value}')
-    print(f'statistic: {statistic}')
-    print(f'degrees of freedom: {dof}')
--- src/test/scripts/umat_with_sampling_test.py
+++
@@ -1,5 +0,0 @@
-from hwetests import umat_with_uncertainty
-
-if __name__ == '__main__':
-    p_value = umat_with_uncertainty.full_algorithm(file_path='../data/ambiguous_data.csv')
-    print(f'p-value: {p_value}')
